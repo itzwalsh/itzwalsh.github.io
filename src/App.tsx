0aeaac30119cb4bd2934e2e380c2db14847cea9b
Zack Walsh <itzwalsh@gmail.com>
2023-07-27 12:42:28 -0400

    ccc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Footer from "./components/Footer"
@@ -7,16 +7,14 @@ import Resume from "./pages/Resume"
 function App() {
   return (
     <div className="max-w-8xl min-h-screen bg-darkgray text-white">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-        <div className="mx-0">
-          <Footer />
-        </div>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <div className="mx-0">
+        <Footer />
+      </div>
     </div>
   )
 }
