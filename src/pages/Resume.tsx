43fd171c6cc9829a37a45257b736afcea09c44d9
Zack Walsh <itzwalsh@gmail.com>
2023-08-14 17:14:26 -0400

    fixing resume embed size

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,12 +1,14 @@
 const Resume = () => {
   return (
     <div className="mx-2 text-white md:mx-32">
-      <iframe
-        src="https://drive.google.com/file/d/1Omx1ZiqcFIOjZQDVfrmKmROZsnlVj7g-/preview"
-        width="640"
-        height="480"
-        allow="autoplay"
-      ></iframe>
+      <section className="flex items-center justify-center">
+        <iframe
+          src="https://drive.google.com/file/d/1Omx1ZiqcFIOjZQDVfrmKmROZsnlVj7g-/preview"
+          width="1280"
+          height="960"
+          allow="autoplay"
+        ></iframe>
+      </section>
     </div>
   )
 }
